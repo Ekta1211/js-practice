1bc3ac2d97df158a11acd951ed78ee5237ebb8dd
Ekta <ekta16567@gmail.com>
2024-01-16 17:02:52 +0000

    edit the program

--- practice/test01.js
+++ practice/test01.js
@@ -1,1 +1,12 @@
 console.log("Hello")
+
+const a = 12345;
+let b = "Ekta";
+
+console.table([a,b]);
+
+let name = "ekta"
+let course = "CS"
+
+console.log(`Hello my name is ${name} and i m pursuing ${course}`);
+
