dc5569c5dd3e9d690e02226a58fe661dc6d890a0
Ekta <ekta16567@gmail.com>
2024-01-16 17:10:07 +0000

    maths function

--- practice/test01.js
+++ practice/test01.js
@@ -1,12 +1,23 @@
-console.log("Hello")
+// console.log("Hello")
 
 const a = 12345;
 let b = "Ekta";
 
-console.table([a,b]);
+// console.table([a,b]);
 
 let name = "ekta"
 let course = "CS"
 
-console.log(`Hello my name is ${name} and i m pursuing ${course}`);
+// console.log(`Hello my name is ${name} and i m pursuing ${course}`);
 
+// math lib
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+
+let max = 6
+let min = 1
+
+// for dice game or any other no game ==> formula used
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
